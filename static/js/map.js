96dbb41e0fed108a04ed2f3b4392cdcbe93d09d6
Mora <moragodofthunder@gmail.com>
2022-10-28 16:13:46 -0700

    Purple pin works for major landmarks, but need Places API for userSearch

--- static/js/map.js
+++ static/js/map.js
@@ -18,19 +18,56 @@ function initMap() {
     geocoder.geocode({ address: userAddress }, (results, status) => {
       if (status === 'OK') {
         // Get the coordinates of the user's location
-        const userLocation = results[0].geometry.location;
+        const userLocation = results[0].geometry.location;    
 
         // // Create a marker
-        // new google.maps.Marker({
-        //   position: userLocation,
-        //   map,
-        // });
+        const centerMarker = new google.maps.Marker({
+          position: userLocation,
+          map,
+          icon: {
+            url: "/static/img/green_pin.png",
+            scaledSize: new google.maps.Size(25, 43)
+          }
+        });
 
         // Zoom in on the geolocated location
         map.setCenter(userLocation);
         map.setZoom(12);
-      } else {
-        alert(`Geocode was unsuccessful for the following reason: ${status}`);
+      } 
+    });
+
+    document.querySelector('#submit-search').addEventListener('click', () => {
+
+      const userSearch = document.querySelector('#place-search').value;
+      console.log("Searched");
+      console.log(userSearch);
+
+      if (userSearch) {
+        const geocoder2 = new google.maps.Geocoder();
+        geocoder2.geocode({ address: userSearch }, (results, status) => {
+          if (status === 'OK') {
+            console.log("Result", results);
+            // Get the coordinates of the search user's location
+            const userSearchLocation = results[0].geometry.location;
+            console.log(userSearchLocation);
+            
+            const userMarker = new google.maps.Marker({
+                position: userSearchLocation,
+                map,
+                icon: {
+                  url: "/static/img/purple_pin.png",
+                  scaledSize: new google.maps.Size(25, 43)
+                }
+            });  
+            
+            map.setCenter(userSearchLocation);
+            map.setZoom(12);
+          }
+          else {
+            alert(`Geocode was unsuccessful for the following reason: ${status}`);
+          }
+        });
       }
     });
-  };
+}
+  
--- static/js/map2.js
+++ static/js/map2.js
@@ -93,33 +93,33 @@ function initMap() {
 //       </form>
 //   </div>
 //   `;
-    // autocomplete = new google.maps.places.Autocomplete(
-    //     document.querySelector("place-search").innerText
-    // ), {
-    //     componentRestrictions: {'country': [userCountryCode]},
-    //     fields: ['geometry', 'name'],
-    //     types: ['establishment']
-    // }
+    autocomplete = new google.maps.places.Autocomplete(
+        document.querySelector("place-search").innerText
+    ), {
+        componentRestrictions: {'country': [userCountryCode]},
+        fields: ['geometry', 'name'],
+        types: ['establishment']
+    }
 
-    // autocomplete.addListener("place_changed", () => {
-    //     const place = autocomplete.getPlace();
-    //     new google.maps.Marker({
-    //         position: userLocation.geometry.location,
-    //         title: userLocation.name,
-    //         content: markerInfo,
-    //         map: plannerMap,
+    autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+        new google.maps.Marker({
+            position: userLocation.geometry.location,
+            title: userLocation.name,
+            content: markerInfo,
+            map: plannerMap,
 
-    //     })
+        })
 
-    // userSearchPlace.addListener("submit", () => {
-    //     userSearchPlace.getPlace();
-    //     new google.maps.Marker({
-    //         position: userLocation.geometry.location,
-    //         title: userLocation.name,
-    //         content: markerInfo,
-    //         map: plannerMap,
-    //     })
-    // });
+    userSearchPlace.addListener("submit", () => {
+        userSearchPlace.getPlace();
+        new google.maps.Marker({
+            position: userLocation.geometry.location,
+            title: userLocation.name,
+            content: markerInfo,
+            map: plannerMap,
+        })
+    });
 
     
   };
@@ -234,3 +234,101 @@ function initMap() {
       }
     });
   };
+
+
+  '</div>' +
+  '<div class="itinerary-menu">' +
+      '<form>' +
+          '<label for="in_itinerary">Add to itinerary</label>' +
+          '<select id="in_itinerary" name="in_itinerary">' +
+              '<option value="day-1">Day 1</option>' +
+              '<option value="day-2">Day 2</option>' +
+              '<option value="day-3">Day 3</option>' +
+              '<option value="day-4">Day 4</option>' +
+              '<option value="day-5">Day 5</option>' +
+              '<option value="day-6">Day 6</option>' +
+              '<option value="day-7">Day 7</option>' +
+          '</select>' +
+          '<input type="submit" value="Add to Itinerary">' +        
+      '</form>' +
+  '</div>' +
+  '<div class="notes">' +
+      '<form>' +
+          '<label for="notes">Notes:</label>' +
+          '<input type="text" id="notes" name="notes">' +
+          '<input type="submit" value="Add Special Note">' +
+      '</form>' +
+  '</div>' +
+
+
+
+
+    //Content for inside infoWindow:
+    const markerInfo = 
+    '<div class="category-menu">' +
+          '<label for="category">Choose place category</label>' +
+          '<select id="category" name="category">' +
+              '<option value="breaky">Breakfast spot</option>' +
+              '<option value="lunch">Lunch place</option>' +
+              '<option value="dinner">Dinner spot</option>' +
+              '<option value="streetfood">Street food/snack</option>' +
+              '<option value="dessert">Dessert place</option>' +
+              '<option value="night">Nightlife</option>' +
+              '<option value="bar">Bar or pub</option>' +
+              '<option value="train">Train station</option>' +
+              '<option value="shop">Shopping</option>' +
+              '<option value="books">Bookstore or library</option>' +
+              '<option value="museum">Museum</option>' +
+              '<option value="history">Historical landmark</option>' +
+              '<option value="photo">Photo spot</option>' +
+              '<option value="temple">Temple/shrine/worship</option>' +
+              '<option value="entertain">Entertainment</option>' +
+              '<option value="music">Music venue</option>' +
+              '<option value="bath">Sento/onsen/hotspring</option>' +
+              '<option value="beach">Beach</option>' +
+              '<option value="park">Park</option>' +
+              '<option value="workspace">Workspace</option>' +
+              '<option value="covid">COVID Testing center</option>' +
+          '</select>' +
+          '<button type="button" id="add-place">Add to Saved Places</button>' +        
+      '</form>' +
+  '</div>' +
+  '<div class="itinerary-menu">' +
+      '<form>' +
+          '<label for="in_itinerary">Add to itinerary</label>' +
+          '<select id="in_itinerary" name="in_itinerary">' +
+              '<option value="day-1">Day 1</option>' +
+              '<option value="day-2">Day 2</option>' +
+              '<option value="day-3">Day 3</option>' +
+              '<option value="day-4">Day 4</option>' +
+              '<option value="day-5">Day 5</option>' +
+              '<option value="day-6">Day 6</option>' +
+              '<option value="day-7">Day 7</option>' +
+          '</select>' +
+          '<input type="submit" value="Add to Itinerary">' +        
+      '</form>' +
+  '</div>' +
+  '<div class="notes">' +
+      '<form>' +
+          '<label for="notes">Notes:</label>' +
+          '<input type="text" id="notes" name="notes">' +
+          '<input type="submit" value="Add Special Note">' +
+      '</form>' +
+  '</div>' +
+  `<script>
+    document.querySelector("#add-place").addEventListener("click", (evt) => {
+      evt.preventDefault()
+      console.log("It worked!")
+    });
+  </script>
+  `;
+
+  const infowindow = new google.maps.InfoWindow({
+    content: markerInfo,
+    ariaLabel: userLocation.title
+});
+
+
+//Submit event listener I don't want to lose:
+document.querySelector('#search-bar').addEventListener('submit', (evt) =>
+        evt.preventDefault());
