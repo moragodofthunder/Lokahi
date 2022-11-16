afbb9b1094968cfc74ea612e037e4647196146c6
Mora <moragodofthunder@gmail.com>
2022-11-16 12:07:19 -0800

    Saved places map JS file created along with HTML page

---
+++ static/js/trip_details.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.querySelector("#add-ohana-button").addEventListener('click', () => {
+    
+    document.querySelector("#add-friend-button").style.display = "block"
+})
---
+++ static/js/user_profile.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.querySelector("#add-ohana-button").addEventListener('click', () => {
+    
+    document.querySelector("#friend-search-box").style.display = "block"
+})
