52b69b1fac120878aa0ed0ed3e80d53d52d268fa
Mora <moragodofthunder@gmail.com>
2022-11-16 18:07:54 -0800

    Dictionary of place_pins started

--- static/js/sp-map.js
+++ static/js/sp-map.js
@@ -1,7 +1,36 @@
 'use strict';
 
 let place_pins = {
-    
+    Breakfast: '/static/img/pins/1-breakfast-pin.png',
+    Lunch: '/static/img/pins/2-lunch-pin.png',
+    Dinner: '/static/img/pins/3-dinner-pin.png',
+    Streetfood: '/static/img/pins/4-streetfood-pin.png',
+    Dessert: '/static/img/pins/5-dessert-pin.png',
+    Cafe: '/static/img/pins/6-cafe-pin.png',
+    Nightlife: '/static/img/pins/7-nightlife-pin.png',
+    Bar/Pub: '/static/img/pins/8-bar-pin.png',
+    Shopping: '/static/img/pins/9-shopping-pin.png',
+    Groceries: '/static/img/pins/10-grocery-pin.png',
+    Convenience Store: '/static/img/pins/11-kombini-pin.png',
+    Outdoor Market: '/static/img/pins/12-farmers-market-pin.png',
+    Covered Market: '/static/img/pins/13-bazaar-pin.png',
+    Bookstore/Library: '/static/img/pins/14-bookstore-pin.png',
+    Train Station: ;
+    Museum: ;
+    Landmark: ;
+    Photo Spot: ;
+    Temple/Shrine/Worship: ;
+    Entertainment: ;
+    Music: ;
+    Hot Spring: ;
+    Beach: ;
+    Park: ;
+    Hiking: ;
+    Surfing: ;
+    Workspace: ;
+    COVID Testing: ;
+    Miscellaneous: ;
+
 }
 
 let tripLat = document.querySelector('#trip-lat').value;
