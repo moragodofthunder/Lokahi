027b20d16c70eaa98a0637db8fe104ef7ad1701f
Mora <moragodofthunder@gmail.com>
2022-12-20 12:47:43 -0800

    Modified server but reverted back to original

--- lokahi_server.py
+++ lokahi_server.py
@@ -478,9 +478,7 @@ def get_place_info():
 
     response = requests.get(url, params=payload).json()
     print(jsonify(response))
-    print("**************************************")
     
-    print("**************************************")
     #TO SEE ALL JSON OBJECT DATA:
     # return response
 
